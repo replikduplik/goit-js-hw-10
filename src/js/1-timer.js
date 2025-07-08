db894afbb38d095e45691d2ef950a123da171708
Çağdaş Şahin <3cagdas3@gmail.com>
2025-07-08 12:42:49 +0300

    first.

---
+++ src/js/1-timer.js
@@ -0,0 +1,84 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const datePicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+startBtn.disabled = true;
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
+
+let userSelectedDate = null;
+let timerInterval = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+      startBtn.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr(datePicker, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  datePicker.disabled = true;
+
+  timerInterval = setInterval(() => {
+    const timeLeft = userSelectedDate - new Date();
+
+    if (timeLeft <= 0) {
+      clearInterval(timerInterval);
+      iziToast.success({
+        title: 'Done',
+        message: 'Time is up!',
+        position: 'topRight',
+      });
+      startBtn.disabled = false;
+      datePicker.disabled = false;
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+
+    daysSpan.textContent = days;
+    hoursSpan.textContent = addLeadingZero(hours);
+    minutesSpan.textContent = addLeadingZero(minutes);
+    secondsSpan.textContent = addLeadingZero(seconds);
+  }, 1000);
+});
